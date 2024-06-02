7a7dbb3f809ea2806d48b84cfb8236af7686f842
VEEPforge <epvillanueva1@up.edu.ph>
2024-06-02 10:16:35 +0800

    Updated

--- src/constants/caves.js
+++ src/constants/caves.js
@@ -5,54 +5,14 @@ export const caves = [
 		'coordinates': [14.2631119, 121.5787233]
 	},
 	{
-		'name': 'Cabag Cave',
-		'province': 'Cavite',
-		'coordinates': [14.239726233256526, 120.95207761298605]
-	},
-	{
-		'name': 'Tagong Bato Small Cave',
-		'province': 'Batangas',
-		'coordinates': [13.620724247390939, 121.29924055471592]
-	},
-	{
-		'name': 'Kinabuhayan Cave',
-		'province': 'Quezon',
-		'coordinates': [14.0413604, 121.4477816]
-	},
-	{
-		'name': 'Malaog Cave',
-		'province': 'Quezon',
-		'coordinates': [14.205853609712022, 121.60966326543495]
-	},
-	{
-		'name': 'Sungawan Cave',
-		'province': 'Quezon',
-		'coordinates': [14.051370747775632, 121.65360857885923]
-	},
-	{
-		'name': 'Sibatiya Cave',
-		'province': 'Quezon',
-		'coordinates': [13.9264670, 121.7782500]
-	},
-	{
-		'name': 'Malusak Cave',
-		'province': 'Quezon',
-		'coordinates': [13.971380156649296, 121.97121006777704]
-	},
-	{
-		'name': 'Bosque Cave',
-		'province': 'Quezon',
-		'coordinates': [13.8815390087003, 122.10293288011098]
-	},
-	{
-		'name': 'Anilon Cave',
-		'province': 'Quezon',
-		'coordinates': [14.952548366400482, 122.02617915947653]
-	},
-	{
 		'name': 'Palale Cave',
 		'province': 'Quezon',
 		'coordinates': [14.03151, 121.68561]
+	},
+	{
+		'name': 'Cabag Cave',
+		'province': 'Cavite',
+		'coordinates': [14.239726233256526, 120.95207761298605]
 	},
 	{
 		'name': 'Calinawan Cave',
@@ -60,19 +20,19 @@ export const caves = [
 		'coordinates': [14.552459933104721, 121.3579269174289]
 	},
 	{
+		'name': 'Tagong Bato Small Cave',
+		'province': 'Batangas',
+		'coordinates': [13.620724247390939, 121.29924055471592]
+	},
+	{
+		'name': 'Bosque Cave',
+		'province': 'Quezon',
+		'coordinates': [13.8815390087003, 122.10293288011098]
+	},
+	{
 		'name': 'Pupot Cave',
 		'province': 'Rizal',
 		'coordinates': [14.551795315579275, 121.32359464131619]
-	},
-	{
-		'name': 'Sangab Caves',
-		'province': 'Rizal',
-		'coordinates': [14.629541963372976, 121.33870084280578]
-	},
-	{
-		'name': 'Mystical Cave',
-		'province': 'Rizal',
-		'coordinates': [14.603629466195992, 121.20755154805515]
 	},
 	{
 		'name': 'Pamitinan Cave',
