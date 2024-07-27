cefff7d6321b47435e65121483509617c24312f3
Andrae Cesar Espino <ahespino@up.edu.ph>
2024-07-27 16:23:48 +0800

    Update axios.js

--- src/app/axios.js
+++ src/app/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: 'http://localhost:5050'
-  baseURL: 'https://resonant-figolla-d34712.netlify.app/'
+  baseURL: 'https://caveproject-server-7kbb.onrender.com'
 });
